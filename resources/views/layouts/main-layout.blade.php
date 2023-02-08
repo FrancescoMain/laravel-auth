c0907c04944fefe4226b05ee48a8a89cc0fee0bd
francescoMain <cesaranofrancescomain@gmail.com>
2023-02-08 17:37:11 +0100

    mod main layout

--- resources/views/layouts/main-layout.blade.php
+++ resources/views/layouts/main-layout.blade.php
@@ -4,20 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel Auth</title>
+    <title>Document</title>
 
-    @vite('resources/js/app.js')
-    @yield('head')
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <!-- Usando Vite -->
+    @vite(['resources/js/app.js'])
 </head>
 <body>
-    
     @include('components.header')
-    @include('components.errors')
-
     @yield('content')
-
     @include('components.footer')
-    
 </body>
+</html>
 </html>
