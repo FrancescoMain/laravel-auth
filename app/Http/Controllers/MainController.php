71ea432a947d403fca38c8cfeaa2f07218560a63
francesco <cesaranofrancescomain@gmail.com>
2023-02-09 13:02:40 +0100

    -A show function

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -7,10 +7,19 @@ use App\Models\Project;
 
 class MainController extends Controller
 {
+    //index
     public function home() {
 
         $projects = Project :: all();
 
         return view('pages.home', compact('projects'));
     }
+
+    //show
+    public function projectShow(Project $project){
+
+        return view('pages.projectShow', compact('project'));
+
+    }
+
 }
