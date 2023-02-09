ab94e13c893348235424e2be731ab63c99f654fa
francescoMain <cesaranofrancescomain@gmail.com>
2023-02-09 11:50:00 +0100

    -A project model & factory

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Model\Project;
 
 class ProjectSeeder extends Seeder
 {
@@ -14,6 +15,6 @@ class ProjectSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Project::factory() -> count(20) -> create();
     }
 }
