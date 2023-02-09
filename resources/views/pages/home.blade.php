2ba65720145344eeab0b22e839bebc228c239992
francesco <cesaranofrancescomain@gmail.com>
2023-02-09 15:33:17 +0100

    -A net project link

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -5,6 +5,10 @@
     <h1>CONTENT</h1>
     <div class="card-body">
         <h1>I miei progetti</h1>
+        @guest
+        @else    
+        <a href="{{ route('admin.project.create') }}">CREATE NEW TASK</a>
+        @endguest
         <ul>
             @foreach ($projects as $project)
 
