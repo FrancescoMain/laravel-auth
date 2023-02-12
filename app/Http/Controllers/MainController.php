fb880a2c11da3bcd67da7679af8a197b92902c54
francesco <cesaranofrancescomain@gmail.com>
2023-02-12 12:24:31 +0100

    fix

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -73,11 +73,11 @@ class MainController extends Controller
     public function projectUpdate(Request $request, Project $project) {
 
 	$data = $request -> validate([
-		'name' => 'required|string|max:64|unique:projects,name' . $project -> id,
-		'description' => 'nullable|string',
-        'main_image' => 'nullable|string|unique:projects,main_image',
-        'relase_date' => 'nullable|string|before:tomorrow',
-		'repo_link' => 'required|string|unique:projects,repo_link' . $project -> id,
+            'name' => 'required|string|min:3|max:64|unique:projects,name,' . $project -> id,
+            'description' => 'nullable|string',
+            'main_image' => 'required|image|mimes:jpg,png,jpeg,gif,svg|max:2048',
+            'relase_date' => 'required|date|before:today',
+            'repo_link' => 'required|string|unique:projects,repo_link,' . $project -> id,
 	]);
 
 	$project -> name = $data['name'];
